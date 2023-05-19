01b61204056fa3b2ec9c38d77ca1017e1c9f9ad1
ryzalena <119529549+ryzalena@users.noreply.github.com>
2023-05-20 02:07:05 +0400

    pseudo sequence generator on task#1

---
+++ task1.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<ctime>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int randomNumber[128];
+	ofstream fin("result.txt"); 
+	for (int i = 0; i < 128; i++)
+	{
+		randomNumber[i] = rand() % 2;	
+		cout << randomNumber[i];
+		fin << randomNumber[i];
+		fin << ",";
+	}
+	fin.close();
+	return 0;
+	
+
+}
